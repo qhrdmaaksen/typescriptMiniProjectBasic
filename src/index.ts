55949e39289bb357b4da3107a0c6f7893380be95
vitamin777 <rlaalsdn8@gmail.com>
2022-12-05 22:51:13 +0900

    chore : prettier add, non null 단언 연산자 테스트

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const printModule = (msg: string) => {
+console.log(msg);
+console.log(msg);
+}
+printModule("Hello World")
+
+// non null 단언 연산자
+const btn = document.getElementById("btn")!
+// btn 존재하면 계속 진행하라는 뜻 없다면 진행치 않음
+btn?.addEventListener("click", ()=>{
+	alert("CLICKED!")
+})
