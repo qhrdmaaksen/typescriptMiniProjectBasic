3aef24d4c88e7fb96b8d7d74250605746c170d99
vitamin777 <rlaalsdn8@gmail.com>
2023-02-19 23:01:17 +0900

    feat: 스케쥴 목록을 동적 배열 설정 및 타입정의
    -1- 스케쥴에 필요한 text, completed 객체를 담은 Todo interface 작성
    -2- 상수 todos 생성 후 type 을 인터페이스 Todo 및 배열로 정의
    -3- handleSubmit 에서 createTodo 함수에 newTodo(input value, completed(checkbox)) 를 인자로 보냄
    -4- createTodo 에서 받은 todo 를 newLI 에 todo.text 로 추가해줘서 form submit 하면 이전과 같이 목록 출력되며 배열에 담겨져있도록하였고 앞으로 로컬 스토리지 활용해 동기화 작업 셋팅을 해놨음
    
    (정상 출력및 작동확인)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ console.log(msg);
 }
 printModule("Hello World")
 
+// 각 객체 타입 정의 (타입 별칭) 인터페이스
+interface Todo {
+	text: string,
+	completed: boolean,
+}
+
+// 할일 목록과 배열은 Todo type 의 배열이라고 알려주기 위해 Todo[] 타입을 사용
+const todos: Todo[] = []
+
 // non null 단언 연산자 ! 를 사용하면 값이 존재한다고 타입스크립트에게 알려줌
 const btn = document.getElementById("btn")! as HTMLButtonElement
 // DOM 으로 타입 단언/ 별칭 HTMLInputElement 타입 단언 사용
@@ -14,22 +23,36 @@ const form = document.querySelector("form")!
 
 const list = document.getElementById('todolist')!
 
+
+
 // e: SubmitEvent 는 이벤트 객체를 받아오는데 이벤트 객체는 타입스크립트에서 기본적으로 제공하지 않기때문에 직접 타입을 지정해줘야함
 const handleSubmit = (e: SubmitEvent) => {
 	e.preventDefault()
 	console.log('submit')
-	const newTodoText = input.value
+	// 새로운 할일의 타입을 Todo 로 지정해 타입설정했던 Todo 객체를 생성
+	const newTodo: Todo = {
+		text: input.value,
+		completed: false,
+	}
+	// 새로운 할일을 생성하는 함수 호출
+	createTodo(newTodo)
+	// todos 배열에 newTodo 객체를 추가
+	todos.push(newTodo)
+	input.value = ''
+}
+
+const createTodo = (todo: Todo) => {
 	// 동적 새로운 li 태그 생성
 	const newLI = document.createElement("li")
 	// 동적 생성된 li 태그에 checkbox 생성
 	const checkbox = document.createElement("input")
 	// typescript 가 checkbox 를 checkbox 로 인식하도록 type 설정
 	checkbox.type = 'checkbox'
-	newLI.append(newTodoText)
+	newLI.append(todo.text)
 	newLI.append(checkbox)
 	list.append(newLI)
-	input.value = ''
 }
+
 form.addEventListener('submit', handleSubmit)
 
 // btn 존재하면 계속 진행하라는 뜻 없다면 진행치 않음
